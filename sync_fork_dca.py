3396baa7b12c9375626b4c47836995987a421a52
anujshah <anujshah584@gmail.com>
2021-12-17 17:09:22 +0530

    sync_fork_dca

---
+++ sync_fork_dca.py
@@ -0,0 +1,43 @@
+# check github repository for updates
+# if there's an update, build and push docker image
+# if there's no update, do nothing
+import subprocess
+import time
+
+
+def check_for_github_repo_changes():
+    repo_url = 'https://github.com/xataxxx/freqtrade'
+    branch = 'dca'
+    repo_path = 'freqtrade'
+
+    # get the last commit hash
+    last_commit_hash = subprocess.check_output(['git', 'rev-parse', 'HEAD'], cwd=repo_path).decode('utf-8').strip()
+
+    # get the last commit hash from the remote
+    remote_commit_hash = \
+        subprocess.check_output(['git', 'ls-remote', '--heads', repo_url, branch], cwd=repo_path).decode('utf-8').split(
+            '\t')[0]
+
+    # compare the two
+    if last_commit_hash != remote_commit_hash:
+        # update the local repo
+        subprocess.call(['git', 'fetch', '--all'], cwd=repo_path)
+        subprocess.call(['git', 'checkout', '-f', branch], cwd=repo_path)
+        subprocess.call(['git', 'reset', '--hard', remote_commit_hash], cwd=repo_path)
+        return True
+    else:
+        return False
+
+
+def build_and_push_docker_image():
+    if check_for_github_repo_changes():
+        print('changes found, building and pushing docker image')
+        subprocess.call(['docker', 'build', '-t', 'anujshah1996/freqtrade:latest', 'freqtrade'])
+        subprocess.call(['docker', 'push', 'anujshah1996/freqtrade:latest'])
+
+
+if __name__ == '__main__':
+    #     run this script every hour
+    while True:
+        build_and_push_docker_image()
+        time.sleep(3600)
