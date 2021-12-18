0d81d3608d68bae7ae059926490906206206dc92
anujshah <anujshah584@gmail.com>
2021-12-18 10:09:40 +0530

    update 3commas with tbedit

--- user_data/strategies/ThreeCommasStrategy.py
+++ user_data/strategies/ThreeCommasStrategy.py
@@ -6,6 +6,8 @@ from freqtrade.strategy.interface import IStrategy
 from dotenv import load_dotenv
 from py3cw.request import Py3CW
 
+from user_data.strategies.tbedit import tbedit
+
 load_dotenv()
 
 log = logging.getLogger(__name__)
@@ -14,37 +16,39 @@ log = logging.getLogger(__name__)
 # credits to https://github.com/AlexBabescu
 
 
-class ThreeCommasStrategy(IStrategy):
+class ThreeCommasStrategy(tbedit):
 
     def confirm_trade_entry(self, pair: str, order_type: str, amount: float, rate: float,
                             time_in_force: str, current_time: datetime, **kwargs) -> bool:
-        if hasattr(super(), 'confirm_trade_entry'):
-            raise NotImplementedError('confirm_trade_entry is already implemented, 3commas will not work.')
+        val = super().confirm_trade_entry(pair, order_type, amount, rate, time_in_force, current_time, **kwargs)
 
-        coin, currency = pair.split('/')
+        if val:
+            coin, currency = pair.split('/')
 
-        p3cw = Py3CW(
-            key=os.getenv('P3CW_KEY'),
-            secret=os.getenv('P3CW_SECRET'),
-        )
+            p3cw = Py3CW(
+                key=os.getenv('P3CW_KEY'),
+                secret=os.getenv('P3CW_SECRET'),
+            )
 
-        # action_id comes from params
-        error, data = p3cw.request(
-            entity='bots',
-            action='start_new_deal',
-            action_id=str(os.getenv('P3CW_BOT_ID')),
-            payload={
-                "bot_id": os.getenv('P3CW_BOT_ID'),
-                "pair": f"{currency}_{coin}",
-            },
-        )
+            # action_id comes from params
+            error, data = p3cw.request(
+                entity='bots',
+                action='start_new_deal',
+                action_id=str(os.getenv('P3CW_BOT_ID')),
+                payload={
+                    "bot_id": os.getenv('P3CW_BOT_ID'),
+                    "pair": f"{currency}_{coin}",
+                },
+            )
 
-        log.info(f"{error} {data}")
+            log.info(f"{error} {data}")
 
-        PairLocks.lock_pair(
-            pair=pair,
-            until=datetime.now(timezone.utc) + timedelta(minutes=5),
-            reason="Send 3c buy order"
-        )
+            PairLocks.lock_pair(
+                pair=pair,
+                until=datetime.now(timezone.utc) + timedelta(minutes=5),
+                reason="Send 3c buy order"
+            )
 
-        return False  # we don't want to keep the trade in freqtrade db
+            return True  # we don't want to keep the trade in freqtrade db
+        else:
+            return False
