3a94796dc0a661db41a025bbdda5c48b4fb55b66
anujshah <anujshah584@gmail.com>
2023-09-27 15:27:38 +0000

    New release version

--- user_data/strategies/NostalgiaForInfinityNext.py
+++ user_data/strategies/NostalgiaForInfinityNext.py
@@ -1,17 +1,1 @@
-
-<?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
- "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html>
-  <head>
-    <title>503 Backend unavailable, connection timeout</title>
-  </head>
-  <body>
-    <h1>Error 503 Backend unavailable, connection timeout</h1>
-    <p>Backend unavailable, connection timeout</p>
-    <h3>Error 54113</h3>
-    <p>Details: cache-dfw-kdal2120120-DFW 1695827745 1327941612</p>
-    <hr>
-    <p>Varnish cache server</p>
-  </body>
-</html>
+404: Not Found
